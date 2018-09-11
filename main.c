945b064af3b58311514af31f63e7fb6f7aa1e049
Augusto Maza <36158534+aumaza@users.noreply.github.com>
2018-09-11 07:15:44 -0300

    probando

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <conio.h>
+
+int main()
+{
+
+    char nombre[20];// = {"Mariana Ines"};
+    char apellido[20];// = {"PEREz"};
+    char nombreCompleto[40];
+    int cant;
+    int i;
+
+    printf("\nIngrese nombre: ");
+    fflush(stdin);
+    fgets(nombre, 20, stdin);
+
+
+    printf("\nIngrese apellido: ");
+    fflush(stdin);
+    fgets(apellido, 20, stdin);
+
+
+    strcpy(nombreCompleto, nombre);
+    strcat(nombreCompleto, " ");
+    strcat(nombreCompleto, apellido);
+    strlwr(nombreCompleto);
+    nombreCompleto[0] = toupper(nombreCompleto[0]);
+
+    cant = strlen(nombreCompleto);
+
+    for(i=0; i<cant; i++)
+    {
+        if(nombreCompleto[i] == ' ')
+        {
+            nombreCompleto[i+1] = toupper(nombreCompleto[i+1]);
+        }
+    }
+
+    printf("\n%s ", nombreCompleto);
+    return 0;
+}
